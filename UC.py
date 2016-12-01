9bfa266964ea2c9e0d440ca2f83c16b108304c0a
Alan Heanue <g00318763@gmit.ie>
2016-12-01 04:00:03 +0000

    changed templates and added css

--- UC.py
+++ UC.py
@@ -14,6 +14,11 @@ def add():
     return render_template('add.html')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
